60d24eb7e1ae20f335111b4bf393643ffa3cd313
Hassan <hs542327@gmail.com>
2023-05-14 03:55:56 +0500

    OptVerification

--- client/b2b/src/Router/SellerRoutes.jsx
+++ client/b2b/src/Router/SellerRoutes.jsx
@@ -1,10 +1,16 @@
+import OtpAuth from "../Pages/Auth/OtpAuth";
 import Welcome from "../Pages/Auth/Welcome";
+
 
 const sellerRoutes = [
 
     {
         path: '/',
         element: <Welcome />
+    },
+    {
+        path: '/Otp-auth',
+        element: <OtpAuth />
     },
 
 ]
