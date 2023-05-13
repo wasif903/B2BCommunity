960b87207d18726bf320ca66d59dfd0967537611
wasif903 <wasifmehmood903@gmail.com>
2023-05-14 04:41:38 +0500

    signup with validation and cookie

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,20 +2,38 @@ import express from "express";
 import AuthSchema from "../models/auth.js";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import { body, validationResult } from 'express-validator';
+import cookieParser from 'cookie-parser';
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-    
-    try {
 
-        const isUserExist = await AuthSchema.findOne({email:req.body.email});
+router.post("/register", [
+    // Validation rules for each field
+    body('firstName').notEmpty().withMessage('First name is required'),
+    body('lastName').notEmpty().withMessage('Last name is required'),
+    body('companyName').notEmpty().withMessage('Company name is required'),
+    body('VAT_ID').notEmpty().withMessage('VAT ID is required'),
+    body('addressLine').notEmpty().withMessage('Address line is required'),
+    body('zipCode').notEmpty().withMessage('Zip code is required'),
+    body('city').notEmpty().withMessage('City is required'),
+    body('country').notEmpty().withMessage('Country is required'),
+    body('email').notEmpty().withMessage('Email is required').isEmail().withMessage('Invalid email address'),
+    body('password').notEmpty().withMessage('Password is required').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+], async (req, res) => {
+    try {
+        // Check for validation errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const isUserExist = await AuthSchema.findOne({ email: req.body.email });
 
         if (isUserExist) {
-          res.status(403).json({message:"Email Already Exists"})
+            return res.status(403).json({ message: "Email Already Exists" });
         } else {
-
-            const securedPass = await bcrypt.hash(req.body.password, 10); 
+            const securedPass = await bcrypt.hash(req.body.password, 10);
 
             const createUser = new AuthSchema({
                 firstName: req.body.firstName,
@@ -30,28 +48,30 @@ router.post("/register", async (req, res) => {
                 password: securedPass,
                 roles: req.body.roles,
             });
-        
+
             const saveUser = await createUser.save();
 
-
-            const token = await jwt.sign(
+            const cookie = await jwt.sign(
                 {
-                    email:createUser.email,
-                    userID:createUser.id,
-                    roles:createUser.roles,
-                    invitation:createUser.invitation
+                    email: createUser.email,
+                    userID: createUser.id,
+                    roles: createUser.roles,
+                    invitation: createUser.invitation
+                }, process.env.JWT_SECRET
+            );
 
-                }, process.env.JWT_SECRET)
-    
-            res.status(200).json({ message: "Registered in successfully", saveUser, token:token });
+            // Generate a cookie and set it in the response
+            res.cookie('cookie', cookie, { httpOnly: true });
 
-        } 
-            
+            res.status(200).json({ message: "Registered in successfully", saveUser });
+        }
     } catch (error) {
         res.status(500).json(error);
         console.log(error);
     }
 });
+
+
 
 
 router.post('/login', async (req, res) => {
