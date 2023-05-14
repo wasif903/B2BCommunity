05ec276bf2bf3b04f024d289b57fb15247b76f92
wasif903 <wasifmehmood903@gmail.com>
2023-05-15 01:42:26 +0500

    sidebar

--- client/b2b/src/Components/Sidebar.jsx
+++ client/b2b/src/Components/Sidebar.jsx
@@ -1,11 +1,23 @@
+/* eslint-disable react/prop-types */
+import styles from "./compStyles/sidebar.module.css";
+import Container from "react-bootstrap/esm/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
-function Sidebar() {
-  
-    return (
+const Sidebar = ({ setIsOpen }) => {
+  return (
     <>
-          
+      <Container className={styles.sidebarWrapper}>
+        <Row>
+          <Col>
+            <button onClick={() => setIsOpen(false)} className="mt-2">
+              X
+            </button>
+          </Col>
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
 export default Sidebar;
