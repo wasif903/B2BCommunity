2195d87a616e24910492372f3ad2ab3c6a15b5ec
wasif903 <wasifmehmood903@gmail.com>
2023-05-15 06:31:09 +0500

    header complete

--- client/b2b/src/Components/Header.jsx
+++ client/b2b/src/Components/Header.jsx
@@ -1,8 +1,8 @@
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+// import Nav from "react-bootstrap/Nav";
+// import Navbar from "react-bootstrap/Navbar";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import styles from "./compStyles/header.module.css";
 import myAcc from "../assets/my_acc.png";
@@ -10,14 +10,11 @@ import Logo from "../assets/logo.png";
 import { faCommentDots } from "@fortawesome/free-solid-svg-icons";
 import Sidebar from "./Sidebar";
 import { useState } from "react";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
-
-  // const handleModal = () => {
-  //   setIsOpen(!isOpen);
-  //   console.log(isOpen)
-  // };
 
   const handleModal = () => {
     setIsOpen(true);
@@ -26,42 +23,59 @@ function Header() {
 
   return (
     <>
-      <Navbar collapseOnSelect className={styles.navBar} expand="lg">
+      <div className={styles.navBar}>
         <Container fluid className='"container-lg'>
-          <Navbar.Brand href="#home">
-            <img src={Logo} width="50px" alt="" />
-          </Navbar.Brand>
+          <Row>
+            <Col md="6">
+              <div className="text-md-start text-center">
+                <img src={Logo} width="50px" alt="" />
+              </div>
+            </Col>
 
-          <Nav className="mx-auto text-center">
-            <div className={styles.search_bar}>
-              <input
-                type="text"
-                className={styles.searchInput}
-                placeholder="Search..."
-              />
-              <FontAwesomeIcon icon={faSearch} className={styles.search_icon} />
-            </div>
-          </Nav>
-          <Nav className="d-flex justify-content-center align-items-center">
-            <Nav.Link className="pe-4 d-none d-lg-flex">
-              <img src={myAcc} width="50px" alt="" />
-            </Nav.Link>
-            <Nav.Link className="pe-4">
-              <FontAwesomeIcon
-                icon={faCommentDots}
-                className="text-white fa-lg"
-              />
-            </Nav.Link>
-            <Nav.Link onClick={handleModal} eventKey={2} className="pe-4">
-              <FontAwesomeIcon icon={faBars} className="text-white fa-lg" />
-            </Nav.Link>
-          </Nav>
+            <Col
+              md="6"
+              className="d-flex justify-content-end align-items-center"
+            >
+              <div className="d-flex justify-content-end align-items-center">
+                <div className="mx-5">
+                  <div className={`${styles.search_bar}`}>
+                    <input
+                      type="text"
+                      className={styles.searchInput}
+                      placeholder="Search..."
+                    />
+                    <FontAwesomeIcon
+                      icon={faSearch}
+                      className={styles.search_icon}
+                    />
+                  </div>
+                </div>
 
-          <Nav.Link className="d-flex d-lg-none pe-4">
-            <img src={myAcc} width="50px" alt="" />
-          </Nav.Link>
+                <div className="pe-lg-4 pe-2 d-none d-lg-flex">
+                  <img src={myAcc} width="50px" alt="" />
+                </div>
+                <div className="pe-lg-4 pe-2">
+                  <FontAwesomeIcon
+                    icon={faCommentDots}
+                    className="text-white fa-lg"
+                  />
+                </div>
+                <div
+                  onClick={handleModal}
+                  // eventKey={2}
+                  className={`${isOpen ? "d-none" : "d-flex"} pe-lg-4 ps-2`}
+                >
+                  <FontAwesomeIcon icon={faBars} className="text-white fa-lg" />
+                </div>
+
+                <div className="d-flex d-lg-none pe-lg-4 ps-2 ps-sm-0">
+                  <img src={myAcc} width="50px" alt="" />
+                </div>
+              </div>
+            </Col>
+          </Row>
         </Container>
-      </Navbar>
+      </div>
 
       {/* Sidebar */}
       {isOpen ? <Sidebar setIsOpen={setIsOpen} /> : ""}
