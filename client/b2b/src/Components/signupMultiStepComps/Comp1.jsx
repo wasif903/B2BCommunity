d394e3e83189a425a3f3afe4323f7b2e2809d823
wasif903 <wasifmehmood903@gmail.com>
2023-05-18 10:18:30 -0700

    get changes

--- client/b2b/src/Components/signupMultiStepComps/Comp1.jsx
+++ client/b2b/src/Components/signupMultiStepComps/Comp1.jsx
@@ -1,11 +1,22 @@
-
+import { useState } from "react"
 
 function Comp1() {
+
+  const [step1 , setStep1] = useState({
+    name1:"",
+    email1:"",
+    phone1:"",
+  })
+
+  const onChange = (e) => {
+    setStep1({...step1, [e.target.name]:e.target.value});
+  }
+
   return (
     <>
-        <input type='text' placeholder="1" />
-        <input type='text' placeholder="1" />
-        <input type='text' placeholder="1" />
+        <input type='text' onChange={onChange} name="name1" value={step1.name1} placeholder="1" />
+        <input type='text' onChange={onChange} name="email1" value={step1.email1} placeholder="1" />
+        <input type='text' onChange={onChange} name="phone1" value={step1.phone1} placeholder="1" />
     </>
   )
 }
