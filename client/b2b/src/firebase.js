c366bc0e4d7652fb9b914a90cbad072a80fda867
shaka912 <khalnayek15@gmail.com>
2023-06-09 00:52:35 +0500

    added firebase authentication

---
+++ client/b2b/src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBswS49wBKaGzab2IzNm94EsE5Nr0Pdd0A",
+  authDomain: "outlet34-810ca.firebaseapp.com",
+  projectId: "outlet34-810ca",
+  storageBucket: "outlet34-810ca.appspot.com",
+  messagingSenderId: "503842684787",
+  appId: "1:503842684787:web:c9650a817c4ae133310f06",
+  measurementId: "G-VFLM987TC0"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
