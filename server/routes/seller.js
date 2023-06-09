f29385f5f885aa0e23076b1c7a2cd95ae0dadcb9
shaka912 <khalnayek15@gmail.com>
2023-06-09 22:55:33 +0500

    (seller) added route for accepting user into group

--- server/routes/seller.js
+++ server/routes/seller.js
@@ -12,7 +12,7 @@ router.post("/addpost", async (req, res) => {
   const title = req.body.title;
   const description = req.body.description;
   const media = req.body.media;
-  
+
   try {
     const user = await userdetails.findOne({ userid: req.body.userid });
     if (!user) {
@@ -32,17 +32,53 @@ router.post("/addpost", async (req, res) => {
         .json({ error: "User is not an admin of the group" });
     }
     const post = await Post.create({
-        title: title,
-        description: description,
-        media: media,
-        group: groupid,
-        author: userid,
-    })
+      title: title,
+      description: description,
+      media: media,
+      group: groupid,
+      author: userid,
+    });
     res.status(200).json({ post });
-}catch (error) {
+  } catch (error) {
     console.log(error);
     res.status(500).send("Server Error");
   }
 });
-
+//route for accepting user into group
+router.post("/acceptuser", async (req, res) => {
+  const groupid = req.body.groupid;
+  const userid = req.body.userid;
+  try {
+    const group = await Group.findById(groupid);
+    if (!group) {
+      res.status(400).send("Group not found");
+    }
+    // Check if the user is an admin of the group
+    const isAdmin = group.admins.some(
+      (adminId) => adminId.toString() === userid
+    );
+    if (!isAdmin) {
+      return res
+        .status(403)
+        .json({ error: "User is not an admin of the group" });
+    }
+    const user = await userdetails.findOne({ userid: req.body.userid });
+    if (!user) {
+      res.status(400).send("User not found");
+    }
+    const isMember = group.members.some(
+      (memberId) => memberId.toString() === userid
+    );
+    if (isMember) {
+      return res
+        .status(403)
+        .json({ error: "User is already a member of the group" });
+    }
+    group.members.push(userid);
+    res.status(200).json({ message:"user Added to group successfully" });
+  }catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
 export default router;
